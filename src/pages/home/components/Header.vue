87cb52d5d0a0d2d18fd178d6030fa2dd1ac1b24c
Marz-M <meiyijie22@outlook.com>
2019-05-02 17:24:01 +0800

    project init

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="header">
+   <div class="header-left">返回</div>
+   <div class="header-input">输入城市景点游玩主题</div>
+   <div class="header-right">城市</div>
+</div>
+</template>
+
+<script>
+export default {
+name:' HomeHeader'
+}
+</script>
+
+<style  scoped>
+.header{
+    display: flex;
+    line-height: 43px;
+    background-color: #00bcd4;
+    color:#fff
+}
+.header-left{
+    width: 43px;
+    float: left;
+}
+.header-input{
+    flex: 1;
+    height: 32px;
+    line-height: 32px;
+    background: #fff;
+    border-radius: 5px;
+    margin-top: 6px;
+    margin-left: 5px;
+    color:#ccc;
+}
+.header-right{
+    width: 62px;
+    float: right;
+    text-align: center;
+}
+</style>
+
+
+
