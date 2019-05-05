3d9c70f06ec450e04f62b1a5d5ba377a28819feb
Marz-M <meiyijie22@outlook.com>
2019-05-05 20:22:03 +0800

    add header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,11 @@
 <template>
 <div class="header">
-   <div class="header-left">返回</div>
-   <div class="header-input">输入城市景点游玩主题</div>
-   <div class="header-right">城市</div>
+   <div class="header-left">
+       <div class="iconfont back-icon">&#xe624;</div>
+   </div>
+   <div class="header-input"> <span class="iconfont">&#xe632;</span>输入城市景点游玩主题</div>
+   <div class="header-right">
+       城市<span class="iconfont arrow-icon">&#xe64a;</span></div>
 </div>
 </template>
 
@@ -13,15 +16,20 @@ name:' HomeHeader'
 </script>
 
 <style  scoped>
+/* @import '~styles/varibles.styl'; */
 .header{
     display: flex;
     line-height: 43px;
     background-color: #00bcd4;
-    color:#fff
+    color:#fff;
 }
 .header-left{
     width: 43px;
     float: left;
+}
+.back-icon{
+    text-align: center;
+    font-size: 20px;
 }
 .header-input{
     flex: 1;
@@ -32,11 +40,16 @@ name:' HomeHeader'
     margin-top: 6px;
     margin-left: 5px;
     color:#ccc;
+    padding-left: 10px;
 }
 .header-right{
     width: 62px;
     float: right;
     text-align: center;
+}
+.arrow-icon{
+    margin-left: 2px;
+    font-size: 12px;
 }
 </style>
 
