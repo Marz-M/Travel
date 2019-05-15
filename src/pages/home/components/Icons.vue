543529787cf6adc08155285f8b0ae6bdd6baa951
Marz-M <meiyijie22@outlook.com>
2019-05-15 17:31:00 +0800

    icon

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="icons">
+        <div class="icon">
+            <div class="icon-img">
+               <img class="icon-imgcontent" src="https://res.gucci.cn/resources/2019/5/8/15573120135908902_content_LightGray_Vignette_Standard_160x160_1557215103_Vignette_581085XJBIO4318_001_Light.jpg" >
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+
+        <div class="icon">
+            <div class="icon-img">
+               <img class="icon-imgcontent" src="https://res.gucci.cn/resources/2019/5/8/15573120135908902_content_LightGray_Vignette_Standard_160x160_1557215103_Vignette_581085XJBIO4318_001_Light.jpg" >
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+
+        <div class="icon">
+            <div class="icon-img">
+               <img class="icon-imgcontent" src="https://res.gucci.cn/resources/2019/5/8/15573120135908902_content_LightGray_Vignette_Standard_160x160_1557215103_Vignette_581085XJBIO4318_001_Light.jpg" >
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+
+        <div class="icon">
+            <div class="icon-img">
+               <img class="icon-imgcontent" src="https://res.gucci.cn/resources/2019/5/8/15573120135908902_content_LightGray_Vignette_Standard_160x160_1557215103_Vignette_581085XJBIO4318_001_Light.jpg" >
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+
+        <div class="icon">
+            <div class="icon-img">
+               <img class="icon-imgcontent" src="https://res.gucci.cn/resources/2019/5/8/15573120135908902_content_LightGray_Vignette_Standard_160x160_1557215103_Vignette_581085XJBIO4318_001_Light.jpg" >
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+
+        <div class="icon">
+            <div class="icon-img">
+               <img class="icon-imgcontent" src="https://res.gucci.cn/resources/2019/5/8/15573120135908902_content_LightGray_Vignette_Standard_160x160_1557215103_Vignette_581085XJBIO4318_001_Light.jpg" >
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+
+        <div class="icon">
+            <div class="icon-img">
+               <img class="icon-imgcontent" src="https://res.gucci.cn/resources/2019/5/8/15573120135908902_content_LightGray_Vignette_Standard_160x160_1557215103_Vignette_581085XJBIO4318_001_Light.jpg" >
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+
+        <div class="icon">
+            <div class="icon-img">
+               <img class="icon-imgcontent" src="https://res.gucci.cn/resources/2019/5/8/15573120135908902_content_LightGray_Vignette_Standard_160x160_1557215103_Vignette_581085XJBIO4318_001_Light.jpg" >
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+
+
+        
+    </div>
+</template>
+<script>
+export default {
+    name:'HomeIcons'
+    
+}
+</script>
+<style scoped>
+.icons{
+    width: 100%;
+    overflow: hidden;
+    height: 0;
+    padding-bottom: 50%;
+}
+.icon{
+    overflow: hidden;
+    width: 25%;
+    float: left;
+    height: 0;
+    padding-bottom: 25%;
+    position: relative;
+}
+.icon-img{
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 22px;
+    box-sizing: border-box;
+    padding: 5px;
+}
+.icon-imgcontent{
+    height: 100%;
+    display: block;
+    margin: 0 auto;
+}
+.icon-desc{
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 22px;
+    line-height: 22px;
+    color: #333;
+    text-align: center;
+}
+</style>
